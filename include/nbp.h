7aea45adb7085d8e0459204f69a6153d6f3af4d8
Alin Enachescu <alinenachescu033@gmail.com>
2019-06-04 18:06:41 +0300

    undefine NBP_LIBRARY_MAIN

--- include/nbp.h
+++ include/nbp.h
@@ -16,8 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define NBP_LIBRARY_MAIN
-
 #ifndef NBP_LIBRARY_H
 #define NBP_LIBRARY_H
 
