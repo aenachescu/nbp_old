ca14b85060c0d7e632c4a524210780f10b779f93
Alin Enachescu <alinenachescu033@gmail.com>
2019-06-07 18:12:34 +0300

    refactoring test and module structs

--- include/nbp.h
+++ include/nbp.h
@@ -19,18 +19,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef NBP_LIBRARY_H
 #define NBP_LIBRARY_H
 
-typedef struct nbp_test_details_t;
-typedef struct nbp_module_details_t;
+struct nbp_test_details_t;
+struct nbp_module_details_t;
 
-typedef void (*nbp_test_pfn_t)(nbp_test_details_t*);
-typedef void (*nbp_module_pfn_t)(nbp_module_details_t*);
+typedef void (*nbp_test_pfn_t)(struct nbp_test_details_t*);
+typedef void (*nbp_module_pfn_t)(struct nbp_module_details_t*);
 
 typedef void (*nbp_before_test_pfn_t)(void);
 typedef void (*nbp_after_test_pfn_t)(void);
 
-typedef struct {
+typedef void(*nbp_setup_module_pfn_t)(void);
+typedef void(*nbp_teardown_module_pfn_t)(void);
+
+struct nbp_test_details_t {
     const char* testName;
-    nbp_test_pfn_t func;
+    nbp_test_pfn_t testFunc;
+    nbp_before_test_pfn_t beforeTestFunc;
+    nbp_after_test_pfn_t afterTestFunc;
+    struct nbp_module_details_t* module;
     unsigned int passedChecks;
     unsigned int failedChecks;
     unsigned int passedTestAsserts;
@@ -39,15 +45,28 @@ typedef struct {
     unsigned int failedModuleAsserts;
     unsigned int passedAsserts;
     unsigned int failedAsserts;
-} nbp_test_details_t;
+    struct nbp_test_details_t* next;
+    struct nbp_test_details_t* prev;
+};
+typedef struct nbp_test_details_t nbp_test_details_t;
 
-typedef struct {
+struct nbp_module_details_t {
     const char* moduleName;
-} nbp_module_details_t;
+    nbp_module_pfn_t moduleFunc;
+    nbp_setup_module_pfn_t setup;
+    nbp_teardown_module_pfn_t teardown;
+    struct nbp_test_details_t* firstTest;
+    struct nbp_test_details_t* lastTest;
+    struct nbp_module_details_t* firstSubmodule;
+    struct nbp_module_details_t* lastSubmodule;
+    struct nbp_module_details_t* next;
+    struct nbp_module_details_t* prev;
+};
+typedef struct nbp_module_details_t nbp_module_details_t;
 
-void nbp_call_test(nbp_test_pfn_t, const char*, nbp_module_details_t*);
+void nbp_call_test(nbp_test_details_t*, nbp_module_details_t*);
 
-void nbp_call_module(nbp_module_pfn_t, const char*, nbp_module_details_t*);
+void nbp_call_module(nbp_module_details_t*, nbp_module_details_t*);
 
 void nbp_set_before_test_pfn(nbp_before_test_pfn_t);
 void nbp_set_after_test_pfn(nbp_after_test_pfn_t);
@@ -80,24 +99,78 @@ void nbp_set_after_test_pfn(nbp_after_test_pfn_t);
 /*
  * TODO: add docs
  */
-#define NBP_TEST(name) void name(nbp_test_details_t* testDetails)
+#define NBP_TEST(name)                                                         \
+    void name(nbp_test_details_t* testDetails);                                \
+    nbp_test_details_t nbpTestDetails ## name = {                              \
+        .testName               = #name,                                       \
+        .testFunc               = name,                                        \
+        .beforeTestFunc         = 0x0,                                         \
+        .afterTestFunc          = 0x0,                                         \
+        .module                 = 0x0,                                         \
+        .passedChecks           = 0,                                           \
+        .failedChecks           = 0,                                           \
+        .passedTestAsserts      = 0,                                           \
+        .failedTestAsserts      = 0,                                           \
+        .passedModuleAsserts    = 0,                                           \
+        .failedModuleAsserts    = 0,                                           \
+        .passedAsserts          = 0,                                           \
+        .failedAsserts          = 0,                                           \
+        .next                   = 0x0,                                         \
+        .prev                   = 0x0                                          \
+    };                                                                         \
+    void name(nbp_test_details_t* testDetails)
 
 /*
  * TODO: add docs
  */
-#define NBP_CALL_TEST(name) nbp_call_test(name, #name, moduleDetails)
+#define NBP_CALL_TEST(name)                                                    \
+    extern nbp_test_details_t nbpTestDetails ## name;                          \
+    nbp_call_test(& nbpTestDetails ## name, moduleDetails)
 
 /*
  * TODO: add docs
  */
-#define NBP_MODULE(name) void name(nbp_module_details_t* moduleDetails)
+#define NBP_MODULE(name)                                                       \
+    void name(nbp_module_details_t* moduleDetails);                            \
+    nbp_module_details_t nbpModuleDetails ## name = {                          \
+        .moduleName     = #name,                                               \
+        .moduleFunc     = name,                                                \
+        .setup          = 0x0,                                                 \
+        .teardown       = 0x0,                                                 \
+        .firstTest      = 0x0,                                                 \
+        .lastTest       = 0x0,                                                 \
+        .firstSubmodule = 0x0,                                                 \
+        .lastSubmodule  = 0x0,                                                 \
+        .next           = 0x0,                                                 \
+        .prev           = 0x0,                                                 \
+    }                                                                          \
+    void name(nbp_module_details_t* moduleDetails)
+
+/*
+ * TODO: add docs
+ */
+#define NBP_MODULE_METHODS(name, setupFunc, teardownFunc)                      \
+    void name(nbp_module_details_t* moduleDetails);                            \
+    nbp_module_details_t nbpModuleDetails ## name = {                          \
+        .moduleName     = #name,                                               \
+        .moduleFunc     = name,                                                \
+        .setup          = setupFunc,                                           \
+        .teardown       = teardownFunc,                                        \
+        .firstTest      = 0x0,                                                 \
+        .lastTest       = 0x0,                                                 \
+        .firstSubmodule = 0x0,                                                 \
+        .lastSubmodule  = 0x0,                                                 \
+        .next           = 0x0,                                                 \
+        .prev           = 0x0,                                                 \
+    }                                                                          \
+    void name(nbp_module_details_t* moduleDetails)
 
 /*
  * TODO: add docs
  */
 #define NBP_CALL_MODULE(name)                                                  \
-    NBP_MODULE(name);                                                          \
-    nbp_call_module(name, ##name, moduleDetails)
+    extern nbp_module_details_t nbpModuleDetails ## name;                      \
+    nbp_call_module(& nbpModuleDetails ## name, moduleDetails)
 
 /*
  * TODO: add docs
@@ -108,16 +181,6 @@ void nbp_set_after_test_pfn(nbp_after_test_pfn_t);
  * TODO: add docs
  */
 #define NBP_CALL_AFTER_TEST(func) nbp_set_after_test_pfn(func)
-
-/*
- * TODO: add docs
- */
-#define NBP_CALL_BEFORE_MODULE(func) nbp_set_before_module_pfn(func)
-
-/*
- * TODO: add docs
- */
-#define NBP_CALL_AFTER_MODULE(func) nbp_set_after_module_pfn(func)
 
 /*
  * TODO: add docs
@@ -173,14 +236,12 @@ void nbp_set_after_test_pfn(nbp_after_test_pfn_t func)
     nbpAfterTestPfn = func;
 }
 
-void nbp_call_test(nbp_test_pfn_t test, const char* testName,
-    nbp_module_details_t* moduleDetails)
+void nbp_call_test(nbp_test_details_t* test, nbp_module_details_t* module)
 {
     return;
 }
 
-void nbp_call_module(nbp_module_pfn_t module, const char* moduleName,
-    nbp_module_details_t* moduleDetails)
+void nbp_call_module(nbp_module_details_t* module, nbp_module_details_t* parent)
 {
     return;
 }
