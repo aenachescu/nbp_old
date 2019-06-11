97e1909425a387603f9e8d156715c8fdae0139c2
EnachescuAlin <alinenachescu033@gmail.com>
2019-06-11 10:54:07 +0300

    added sample for submodules in the same file

---
+++ samples/submodules_one_file/submodules_one_file_sample_test.c
@@ -0,0 +1,151 @@
+/*
+No Bugs in Production (NBP)
+Copyright (C) 2019-present Alin Enachescu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define NBP_LIBRARY_MAIN
+#include <nbp.h>
+
+NBP_MAIN_MODULE(submodules_one_file_sample)
+{
+    NBP_CALL_MODULE(module1);
+    NBP_CALL_MODULE(module2);
+}
+
+NBP_MODULE(module1)
+{
+    NBP_CALL_MODULE(submodule1);
+    NBP_CALL_MODULE(submodule2);
+}
+
+NBP_MODULE(module2)
+{
+    NBP_CALL_MODULE(submodule3);
+    NBP_CALL_MODULE(submodule4);
+}
+
+NBP_MODULE(submodule1)
+{
+    NBP_CALL_TEST(test1);
+    NBP_CALL_TEST(test2);
+    NBP_CALL_TEST(test3);
+    NBP_CALL_TEST(test4);
+}
+
+NBP_MODULE(submodule2)
+{
+    NBP_CALL_TEST(test5);
+    NBP_CALL_TEST(test6);
+    NBP_CALL_TEST(test7);
+    NBP_CALL_TEST(test8);
+}
+
+NBP_MODULE(submodule3)
+{
+    NBP_CALL_TEST(test9);
+    NBP_CALL_TEST(test10);
+    NBP_CALL_TEST(test11);
+    NBP_CALL_TEST(test12);
+}
+
+NBP_MODULE(submodule4)
+{
+    NBP_CALL_TEST(test13);
+    NBP_CALL_TEST(test14);
+    NBP_CALL_TEST(test15);
+    NBP_CALL_TEST(test16);
+}
+
+NBP_TEST(test1)
+{
+    NBP_CHECK(1);
+}
+
+NBP_TEST(test2)
+{
+    NBP_CHECK(2);
+}
+
+NBP_TEST(test3)
+{
+    NBP_CHECK(3);
+}
+
+NBP_TEST(test4)
+{
+    NBP_CHECK(4);
+}
+
+NBP_TEST(test5)
+{
+    NBP_CHECK(1);
+}
+
+NBP_TEST(test6)
+{
+    NBP_CHECK(2);
+}
+
+NBP_TEST(test7)
+{
+    NBP_CHECK(3);
+}
+
+NBP_TEST(test8)
+{
+    NBP_CHECK(4);
+}
+
+NBP_TEST(test9)
+{
+    NBP_CHECK(1);
+}
+
+NBP_TEST(test10)
+{
+    NBP_CHECK(2);
+}
+
+NBP_TEST(test11)
+{
+    NBP_CHECK(3);
+}
+
+NBP_TEST(test12)
+{
+    NBP_CHECK(4);
+}
+
+NBP_TEST(test13)
+{
+    NBP_CHECK(1);
+}
+
+NBP_TEST(test14)
+{
+    NBP_CHECK(2);
+}
+
+NBP_TEST(test15)
+{
+    NBP_CHECK(3);
+}
+
+NBP_TEST(test16)
+{
+    NBP_CHECK(4);
+}
+
