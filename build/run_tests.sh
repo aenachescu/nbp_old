ae7dc91f887e24589903599e671773f8057475cb
Alin Enachescu <alinenachescu033@gmail.com>
2019-06-19 16:09:22 +0300

    refactoring run_tests.sh; check if sample output is the expected printer output

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -14,50 +14,54 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+status=0
+
+function run_test {
+    echo "running test $1"
+
+    sample=${1%_sample}
+
+    cd ../samples/$sample
+    expected_output=$(<expected_linux_printer_output.txt)
+    cd ../../bin
+
+    cd $1
+    output=$(./$1)
+    testStatus=$?
+    cd ..
+
+    if [ $testStatus -ne $2 ]; then
+        echo "expected status: $2"
+        echo "current status: $testStatus"
+        echo $'test failed\n'
+        status=1
+        return
+    fi
+
+    if [ "$expected_output" == "$output" ]; then
+        echo $'test passed\n'
+    else
+        echo "$expected_output"
+        echo "$output"
+        echo $'test failed\n'
+        status=1
+    fi
+}
+
 cd ../bin
 
-cd basic_sample
-./basic_sample
-cd ..
-
-cd modules_sample
-./modules_sample
-cd ..
-
-cd modules_one_file_sample
-./modules_one_file_sample
-cd ..
-
-cd submodules_sample
-./submodules_sample
-cd ..
-
-cd submodules_one_file_sample
-./submodules_one_file_sample
-cd ..
-
-cd main_module_fixtures_sample
-./main_module_fixtures_sample
-cd ..
-
-cd main_module_fixtures_one_file_sample
-./main_module_fixtures_one_file_sample
-cd ..
-
-cd main_module_setup_sample
-./main_module_setup_sample
-cd ..
-
-cd main_module_setup_one_file_sample
-./main_module_setup_one_file_sample
-cd ..
-
-cd main_module_teardown_sample
-./main_module_teardown_sample
-cd ..
-
-cd main_module_teardown_one_file_sample
-./main_module_teardown_one_file_sample
-cd ..
+run_test basic_sample 0
+run_test modules_sample 0
+run_test modules_one_file_sample 0
+run_test submodules_sample 0
+run_test submodules_one_file_sample 0
+run_test main_module_fixtures_sample 0
+run_test main_module_fixtures_one_file_sample 0
+run_test main_module_setup_sample 0
+run_test main_module_setup_one_file_sample 0
+run_test main_module_teardown_sample 0
+run_test main_module_teardown_one_file_sample 0
 
 cd ../build
+
+exit $status
