f32949c8e691d2204ae88d4962a9eda9befafc2a
Alin Enachescu <alinenachescu033@gmail.com>
2019-08-04 14:21:55 +0300

    added macro for no error; added macros for errors related to event structure

--- include/private/types/errors.h
+++ include/private/types/errors.h
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NBP_ERROR_CONTEXT_STRING                                (int) 1
 #define NBP_ERROR_CONTEXT_CUSTOM                                (int) 2
 
+#define NBP_NO_ERROR                                            (int) 0
 #define NBP_ERROR_ALLOC                                         (int) 1
 #define NBP_ERROR_TEST_ALREADY_CALLED                           (int) 2
 #define NBP_ERROR_MODULE_ALREADY_CALLED                         (int) 3
@@ -33,9 +34,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NBP_ERROR_INVALID_TEST_STATE                            (int) 8
 #define NBP_ERROR_INVALID_MODULE_STATE                          (int) 9
 #define NBP_ERROR_FAILED_TO_INIT_EVENT                          (int) 10
-#define NBP_ERROR_TEST_FLAGS_INVALID_VALUE                      (int) 11
-#define NBP_ERROR_MODULE_FLAGS_INVALID_VALUE                    (int) 12
-#define NBP_ERROR_INVALID_MODULE_STATS                          (int) 13
+#define NBP_ERROR_FAILED_TO_UNINIT_EVENT                        (int) 11
+#define NBP_ERROR_FAILED_TO_WAIT_EVENT                          (int) 12
+#define NBP_ERROR_FAILED_TO_SIGNAL_EVENT                        (int) 13
+#define NBP_ERROR_TEST_FLAGS_INVALID_VALUE                      (int) 14
+#define NBP_ERROR_MODULE_FLAGS_INVALID_VALUE                    (int) 15
+#define NBP_ERROR_INVALID_MODULE_STATS                          (int) 16
 
 struct nbp_error_t {
     int errorCode;
