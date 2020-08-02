2933330912afb75c88fb4f826f418a0cb3af4e6a
Alin Enachescu <alinenachescu033@gmail.com>
2020-08-02 12:40:15 +0300

    added mt_scheduler_run_empty_module_after_test sample

--- build/LinuxCMake/SamplesConfig.cmake
+++ build/LinuxCMake/SamplesConfig.cmake
@@ -47,6 +47,7 @@ list(APPEND SAMPLES basic
     mt_scheduler_run_module_after_empty_module
     mt_scheduler_run_module_before_empty_module
     mt_scheduler_run_module_on_same_thread_with_empty_module
+    mt_scheduler_run_empty_module_after_test
 )
 
 # Custom flags lists for targets
@@ -96,6 +97,7 @@ list(APPEND SAMPLES_CFLAGS ""
     ${MT_SCHEDULER_FLAGS}
     ${MT_SCHEDULER_FLAGS}
     ${MT_SCHEDULER_FLAGS}
+    ${MT_SCHEDULER_FLAGS}
 )
 
 list (APPEND SAMPLES_LFLAGS ""
@@ -137,6 +139,7 @@ list (APPEND SAMPLES_LFLAGS ""
     ""
     ${MT_SCHEDULER_LINK_FLAGS}
     ""
+    ${MT_SCHEDULER_LINK_FLAGS}
     ${MT_SCHEDULER_LINK_FLAGS}
     ${MT_SCHEDULER_LINK_FLAGS}
     ${MT_SCHEDULER_LINK_FLAGS}
