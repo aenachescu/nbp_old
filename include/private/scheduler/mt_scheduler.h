e18366202cd2e952384319a58e7840846a5f4095
Alin Enachescu <alinenachescu033@gmail.com>
2020-08-23 18:58:31 +0300

    :wrench: :hammer: used NBP_PP_VARCOUNT macro

--- include/private/scheduler/mt_scheduler.h
+++ include/private/scheduler/mt_scheduler.h
@@ -181,18 +181,6 @@ void* nbp_mt_scheduler_create_ctx(
 // it's used when NBP_MT_SCHEDULER_CTX is called without rules
 #define NBP_MT_SCHEDULER_PRIVATE_PP_EAT_P_
 
-#define NBP_MT_SCHEDULER_PRIVATE_PP_VARCOUNT(...)                              \
-    NBP_MT_SCHEDULER_PRIVATE_PP_VARCOUNT_HELPER(                               \
-        P ## __VA_ARGS__,                                                      \
-        32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15,\
-        14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,                         \
-    )
-
-#define NBP_MT_SCHEDULER_PRIVATE_PP_VARCOUNT_HELPER(                           \
-    _, _32, _31, _30, _29, _28, _27, _26, _25, _24, _23, _22, _21, _20, _19,   \
-    _18, _17, _16, _15, _14, _13, _12, _11, _10, _9, _8, _7, _6, _5, _4, _3,   \
-    _2, _n, ...) _n
-
 #define NBP_MT_SCHEDULER_PRIVATE_PP_CONCAT(a, b)                               \
     NBP_MT_SCHEDULER_PRIVATE_PP_CONCAT_HELPER(a, b)
 
@@ -297,7 +285,7 @@ void* nbp_mt_scheduler_create_ctx(
 #define NBP_MT_SCHEDULER_PRIVATE_PP_EAT_PP_NBP_MT_SCHEDULER_CTX(...)           \
     NBP_MT_SCHEDULER_PRIVATE_PP_CONCAT(                                        \
         NBP_MT_SCHEDULER_PRIVATE_PP_PROCESSING_RULE_,                          \
-        NBP_MT_SCHEDULER_PRIVATE_PP_VARCOUNT(P_ ## __VA_ARGS__)                \
+        NBP_PP_VARCOUNT(P_ ## __VA_ARGS__)                                     \
     )(P_ ## __VA_ARGS__)
 
 #define NBP_SCHEDULER_PREPROCESSING_CONTEXT(ctx)                               \
